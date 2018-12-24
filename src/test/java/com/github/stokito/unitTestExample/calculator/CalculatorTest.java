6169624ff39c4c2cb38c93365b621546bba866b0
Karthik Reddy <karthikr@STI.COM>
2018-12-24 19:18:38 +0530

    added version

--- src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
+++ src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
@@ -4,24 +4,40 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static int minus(int a, int b) {
+		return a - b;  
+	}
+
+	public static int divide(int a, int b) {
+		return a / b;
+	}
+
+	public static  int multiply(int a, int b) {
+		return a * b;
+	}
 
 	@Test
 	public void testSum() {
-		Calculator calculator = new Calculator();
+	//	Calculator calculator = new Calculator();
 		
-		Assert.assertEquals(4, calculator.sum(2, 2));
+		Assert.assertEquals(4, sum(2, 2));
 	}
 
 	@Test
 	public void testMinus() {
-		Calculator calculator = new Calculator();
-		Assert.assertEquals(1, calculator.minus(2, 1));
+		//Calculator calculator = new Calculator();
+		Assert.assertEquals(1, minus(2, 1));
 	}
 
 	@Test
 	public void testDivide() {
-		Calculator calculator = new Calculator();
-		Assert.assertEquals(2, calculator.divide(6, 3));
+		//Calculator calculator = new Calculator();
+		Assert.assertEquals(2, divide(6, 3));
 	}
 
 	/*@Test(expected = ArithmeticException.class)
